fe5db3034230e66c6314b0c53c6ca07899998ab9
Mike Wooldridge <mike@hellion.org>
2016-06-06 19:43:57 -0700

    Test script

---
+++ index.js
@@ -0,0 +1,70 @@
+var Kerberos = require('kerberos').Kerberos;
+var kerberos = new Kerberos();
+var http = require('http');
+
+function httpget(opts, callback) {
+  console.log('submitting to '+(opts.hostname||opts.host)+' with authorization header: '+(opts.headers||{}).authorization);
+  var req = http.get(opts, function(res) {
+    if (res.statusCode == 401) {
+      console.log(res.headers);
+      submitWithAuthorization(req, opts, callback);
+      return;
+    }
+    callback(res);
+  });
+  return req;
+}
+
+function submitWithAuthorization(oldreq, opts, callback) {
+  kerberos.authGSSClientInit("HTTP@"+(opts.hostname || opts.host), 0, function(err, ctx) {
+    if (err) {
+      throw new Error(""+err);
+    }
+    console.log('done init '+ctx);
+    console.log(JSON.stringify(ctx, null, 2));
+    kerberos.authGSSClientStep(ctx, "", function (err) {
+      if (err) {
+        throw new Error(""+err);
+      }
+      console.log('done step '+ctx.response);
+      var headers = opts.headers || {};
+      headers.authorization = "Negotiate "+ctx.response;
+      opts.headers = headers;
+      var newreq = httpget(opts, callback);
+
+      // tell oldReq "owner" about newReq. resubmit is an "unofficial" event
+      oldreq.emit('resubmit', newreq);
+      kerberos.authGSSClientClean(ctx, function(err) {
+        if (err) {
+          throw new Error(""+err);
+        }
+      });
+    });
+  });
+}
+
+// //////////////////////////////////////////////////////////////////
+
+var options = {
+    hostname : "mwooldri.marklogic.com",
+    //hostname : "mwooldri",
+    port : 8060,
+    path : "/"
+};
+
+var req = httpget(options, function(res) {
+  var body = '';
+  res.on('data', function(chunk) {
+    body += chunk;
+  });
+  res.on('end', function() {
+    console.log("BODY: "+body);
+  });
+});
+
+req.on('resubmit', function(newreq) {
+  console.log('request resubmitted');
+  req = newreq;
+});
+
+return;
