273ea75530553b2341277e520ab065fdbec8bd64
Mike Wooldridge <mike@hellion.org>
2016-06-09 11:04:26 -0700

    example using request-promise

---
+++ request-promise.js
@@ -0,0 +1,64 @@
+var rp = require('request-promise'),
+    Kerberos = require('kerberos').Kerberos,
+    kerberos = new Kerberos();
+
+var options = {
+  method: 'GET',
+  uri: 'http://mwooldri.marklogic.com:8060/file.html',
+  headers: {},
+  json: true
+};
+
+function getMethods(obj) {
+  var result = [];
+  for (var id in obj) {
+    try {
+      //if (typeof(obj[id]) == "function") {
+        result.push(id + ": " + obj[id].toString());
+      //}
+    } catch (err) {
+      result.push(id + ": inaccessible");
+    }
+  }
+  return result;
+}
+
+function request(options) {
+  console.log('Getting ' + options.uri + ' with authorization header: ' + options.headers.authorization);
+  rp(options)
+    .then(function (response) {
+      console.log('Response: ');
+      console.log(response);
+    })
+    .catch(function (err) {
+      console.log('Error statusCode: '+err.statusCode);
+      console.log('Header www-authenticate: '+err.response.headers['www-authenticate']);
+      if (err.statusCode === 401) {
+        kerberos.authGSSClientInit("HTTP@"+"mwooldri.marklogic.com", 0, function(err, ctx) {
+          if (err) {
+            throw new Error(""+err);
+          }
+          console.log(getMethods(ctx).join("\n"));
+          console.log(typeof ctx);
+          kerberos.authGSSClientStep(ctx, "", function (err) {
+            if (err) {
+              throw new Error(""+err);
+            }
+            console.log(getMethods(ctx).join("\n"));
+            console.log('Finished step '+ctx);
+            var headers = options.headers || {};
+            headers['authorization'] = "Negotiate "+ctx.response;
+            options.headers = headers;
+            request(options);
+            kerberos.authGSSClientClean(ctx, function(err) {
+              if (err) {
+                throw new Error(""+err);
+              }
+            });
+          });
+        });
+      }
+    });
+}
+
+request(options);
